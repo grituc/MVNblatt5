a430f7e4d7b1085072de082e5745768f8d60f5db
Eugeniu Grituc <eugen.grituc@gmail.com>
2016-06-19 14:57:42 +0200

    some minor changes (working on the tests using a Stub-Object)

--- src/main/java/de/hfu/meldeauskunft/ResidentRepositoryStub.java
+++ src/main/java/de/hfu/meldeauskunft/ResidentRepositoryStub.java
@@ -1,12 +1,21 @@
 package de.hfu.meldeauskunft;
 
-import java.util.List;
+import java.util.*;
 
 public class ResidentRepositoryStub implements ResidentRepository{
 	private List<Resident> residents;
 	
-	public ResidentRepositoryStub() {	
-		residents = null;
+	public ResidentRepositoryStub() {
+		residents = new ArrayList<Resident>();
+		/*
+		Resident r1, r2, r3;
+		r1 = new Resident("Donald", "Duck", "Grieshaberstr. 7", "Furtwangen", null);
+		r2 = new Resident("Daisie", "Duck", "Wilhelmstr. 11", "Furtwangen", null);
+		r3 = new Resident("George", "Martin", "Blumentstr. 11", "Villingen", null);
+		addResident(r1);
+		addResident(r2);
+		addResident(r3);
+		 */	
 	}
 	
 	public void addResident(Resident r){
--- src/test/java/de/hfu/BaseResidentServiceTest.java
+++ src/test/java/de/hfu/BaseResidentServiceTest.java
@@ -1,11 +1,9 @@
 package de.hfu;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import de.hfu.meldeauskunft.*;
 
-import java.util.List;
-
-import de.hfu.meldeauskunft.Resident;
-import de.hfu.meldeauskunft.ResidentServiceException;
+import java.util.*;
 
 /*Integrationstest for the methods   
 *public List<Resident> getFilteredResidentsList(Resident filterResident) and
@@ -15,7 +13,38 @@ import de.hfu.meldeauskunft.ResidentServiceException;
 
 public class BaseResidentServiceTest {
 	@Test
-	public void testGetFilteredResidentsList(){
+	public void test1GetFilteredResidentsList(){
+		BaseResidentService rs = new BaseResidentService();
+		ResidentRepositoryStub rr = new ResidentRepositoryStub();
+		rs.setResidentRepository(rr);
+	
+		Resident r1, r2, r3, r4;
+		r1 = new Resident("Donald", "Duck", "Grieshaberstr. 7", "Furtwangen", null);
+		r2 = new Resident("Daisie", "Duck", "Wilhelmstr. 11", "Furtwangen", null);
+		r3 = new Resident("George", "Martin", "Blumentstr. 11", "Villingen", null);
+		r4 = new Resident(null, "Duck", null, null, null);
+
+		rr.addResident(r1);
+		rr.addResident(r2);
+		rr.addResident(r3);
+
+		ArrayList<Resident> testList = new ArrayList<Resident>();
+		testList.add(r1);
+		testList.add(r2);
+		
+		assertEquals(testList, rs.getFilteredResidentsList(r4));
+		//assertE
+		
+		
+	}
+	
+	@Test
+	public void test2GetFilteredResidentsList(){
+		
+	}
+	
+	@Test
+	public void test3GetFilteredResidentsList(){
 		
 	}
 
